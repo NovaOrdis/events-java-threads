0e3324f57ac9583ca68f2c8859e78f7478ecccdd
Ovidiu Feodorov <ofeodorov@apple.com>
2014-08-04 21:38:30 -0700

    modified install and run wrapper scripts to run on Mac

--- util/install-locally.sh
+++ util/install-locally.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-rm -rf /c/projects/runtime/tda*
+if [ "${RUNTIME_DIR}" = "" ]; then
+    echo "'RUNTIME_DIR' environment variable not set. Set it and try again" 1>&2;
+    exit 1
+fi
 
-unzip ./target/tda-1.0.zip -d /c/projects/runtime
+if [ ! -d ${RUNTIME_DIR} ]; then
+    echo "'RUNTIME_DIR' ${RUNTIME_DIR} does not exist" 1>&2;
+    exit 2
+fi
 
-(cd /c/projects/runtime; ln -s ./tda-1.0 tda)
+rm -rf ${RUNTIME_DIR}/tda*
+
+unzip ./target/tda-1.0.zip -d ${RUNTIME_DIR}
+
+(cd ${RUNTIME_DIR}; ln -s ./tda-1.0 tda)
+
+chmod a+rx ${RUNTIME_DIR}/tda/bin/*
+
 
